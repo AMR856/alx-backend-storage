79034476daa5246d1ac1e6e5589d055b63bdb6b0
ameralnas <amer.live477@gmail.com>
2024-06-13 19:38:43 +0300

    Finally back

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- A script to create I don't what is that
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US';
+);
