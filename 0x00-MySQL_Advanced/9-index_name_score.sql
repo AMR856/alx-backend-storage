597e3246f175e677f1ed935bacb642710206d6ff
ameralnas <amer.live477@gmail.com>
2024-06-14 04:21:29 +0300

    hahahha

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,2 @@
 -- Some indexes
-ALTER TABLE names 
-ADD COLUMN score_string VARCHAR(255) AS (CONVERT(score, CHAR));
-CREATE INDEX idx_name_first_score ON names (name(1), score_string(1));
+CREATE INDEX idx_name_first_score ON names (name(1), score);
